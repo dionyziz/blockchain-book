81846f864bb15c3e25bc8ac4bb02b6dc402884c8
Dionysis Zindros <dionyziz@gmail.com>
2023-02-01 01:37:56 -0800

    Finish Chapter 7

--- plots/convergence-opportunities.py
+++ plots/convergence-opportunities.py
@@ -1,5 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import rc
+
+plt.rcParams['text.latex.preamble'] = r"\usepackage{lmodern}"
+
+rc('text', usetex=True)
+rc(
+    'font',
+    family='serif',
+    serif=['Computer Modern Roman'],
+    monospace=['Computer Modern Typewriter'],
+    size=25
+)
 
 n = 10
 t = 2
@@ -11,7 +23,7 @@ conv_opp = f - f**2
 fig, ax = plt.subplots()
 ax.plot(n * p * q, conv_opp)
 
-ax.set(xlabel='npq', ylabel='# of convergence opportunities per unit of time',
+ax.set(xlabel='$npq$', ylabel='\# of convergence opportunities per unit of time',
        title='Parametrizations for block generation rate')
 ax.grid()
 
